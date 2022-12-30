8ef14e6d34c6c0f81e8b692a49c83aac05e2d348
Anton Vovk <anton.s.vovk@gmail.com>
2022-12-30 21:00:53 +0300

    small update ignore list

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 """Libraries"""
-from os                                     import chdir,path
+from os import chdir, path
+
 from py_scripts.server_connection           import server_connection
+from py_scripts.close_server_connection     import close_server_connection
+from py_scripts.ETL_process                 import ETL_process
+from py_scripts.file_transfer               import file_transfer
 from py_scripts.find_early_date             import find_early_date
+from py_scripts.load_blacklist              import load_blacklist
 from py_scripts.load_data_from_terminals    import load_data_from_terminals
 from py_scripts.load_transactions           import load_transactions
-from py_scripts.load_blacklist              import load_blacklist
-from py_scripts.ETL_process                 import ETL_process
 from py_scripts.report_1                    import report_1
 from py_scripts.report_2                    import report_2
 from py_scripts.report_3                    import report_3
 from py_scripts.report_4                    import report_4
-from py_scripts.file_transfer               import file_transfer
-from py_scripts.close_server_connection     import close_server_connection
 
 def main():
     """run ETL process"""
-    cursor, connection = server_connection()
     chdir('/home/demipt/yupi')
-    X, date = find_early_date()
-    load_data_from_terminals(X, date, cursor)
-    load_transactions(X, cursor)
-    load_blacklist(X, date, cursor)
+    cursor, connection = server_connection()
+    way, date = find_early_date()
+    load_data_from_terminals(way, date, cursor)
+    load_transactions(way, cursor)
+    load_blacklist(way, date, cursor)
     ETL_process(cursor)
     report_1(cursor, date)
     report_2(cursor, date)
     report_3(cursor, date)
     report_4(cursor, date)
-    file_transfer(X)
+    file_transfer(way)
     close_server_connection(cursor, connection)
 
 # Profiler + run
--- py_scripts/ETL_process.py
+++ py_scripts/ETL_process.py
@@ -1,5 +1,5 @@
 # ETL
 def ETL_process(curs):
-    inc = open('sql_scripts\INCREMENTAL.sql').read().split(";")
+    inc = open('sql_scripts/INCREMENTAL.sql','r').read().split(";")
     for i in inc:
     	curs.execute( i )
