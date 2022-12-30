13a33ef258027a8b5ad7321af660a3d4556da34c
Anton Vovk <anton,s,v@mail.ru>
2022-12-30 15:06:57 +0300

    Small changes

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ import jaydebeapi as jdba
 # Установка соединения с сервером
 conn = jdba.connect(
 'oracle.jdbc.driver.OracleDriver',
-'jdbc:oracle:thin:demipt/gandalfthegrey@de-oracle.chronosavant.ru:1521/deoracle',
+'jdbc:oracle:thin:user/password@de-oracle.chronosavant.ru:1521/deoracle',
 ['user','password'],
 '/home/demipt/ojdbc8.jar'
 )
 
 curs = conn.cursor()
-
 
 os.chdir('/home/demipt/yupi')
 
